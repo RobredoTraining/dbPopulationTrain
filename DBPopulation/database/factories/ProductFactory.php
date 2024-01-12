d2fbba8adeaa58fb5e05adb095399981faefde19
Pablo Robredo <pgrobredo7@gmail.com>
2024-01-12 12:40:06 +0100

    first commit

---
+++ DBPopulation/database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()-> name(),
+            'short_description' => fake()-> sentence(),
+            'description' => fake()-> paragraph(),
+            'price' => random_int(1,125)
+
+        ];
+    }
+}
